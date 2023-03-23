876e1a1b05013d6b6eac9c00ad112a6b81673bb9
Dushyantha Kalehewatta <Ulindu.Dushyantha@softlogiclife.lk>
2023-03-23 18:56:57 +0530

    Add project files.

---
+++ JWT_AuthenticationNetcoreWebAPI/Data/DbContextClass.cs
@@ -0,0 +1,23 @@
+﻿using JWT_AuthenticationNetcoreWebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT_AuthenticationNetcoreWebAPI.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<Product> Products
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ JWT_AuthenticationNetcoreWebAPI/Model/JWTTokenResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace JWT_AuthenticationNetcoreWebAPI.Model
+{
+    public class JWTTokenResponse
+    {
+        public string? Token
+        {
+            get;
+            set;
+        }
+    }
+}
